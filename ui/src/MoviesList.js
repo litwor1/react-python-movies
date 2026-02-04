d926fd8db268b1e80c08f3063b54e367dc7fe50f
litwor1 <rzepeckapaulina@gmail.com>
2026-02-04 21:51:58 +0100

    ISSI Lab - enhance movie list styling with animations and header decoration

--- ui/src/MoviesList.js
+++ ui/src/MoviesList.js
@@ -2,6 +2,7 @@ import MovieListItem from "./MovieListItem";
 
 export default function MoviesList(props) {
     return <div>
+        <div className="movies-header-decoration">🎬</div>
         <h2>Movies</h2>
         <ul className="movies-list">
             {props.movies.map(movie => <li key={movie.id}>
